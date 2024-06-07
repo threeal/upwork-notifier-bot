ffab0e8a88445c4ef3e16446a595ea2bb0d41be8
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-06-07 13:35:18 +0700

    feat: handle all errors (#16)
    * feat: handle error during client log in
    
    * feat: handle error during commands register
    
    * feat: handle error during commands execution

--- src/bin.ts
+++ src/bin.ts
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+import { getErrorMessage } from "catched-error-message";
 import { Client, Events, GatewayIntentBits, Routes } from "discord";
 import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
@@ -16,12 +17,17 @@ yargs(hideBin(process.argv))
     const commands = [ListJobsCommand, SubscribeJobsCommand];
 
     client.once(Events.ClientReady, async (client) => {
-      console.log("Client ready!");
+      console.info(`Logged in as ${client.user.tag}!`);
 
-      await client.rest.put(Routes.applicationCommands(client.application.id), {
-        body: commands.map((command) => command.data.toJSON()),
-      });
-      console.log("Commands registered!");
+      try {
+        await client.rest.put(
+          Routes.applicationCommands(client.application.id),
+          { body: commands.map((command) => command.data.toJSON()) },
+        );
+        console.info("Commands registered!");
+      } catch (err) {
+        console.error(`Failed to register commands: ${getErrorMessage(err)}`);
+      }
     });
 
     client.on(Events.InteractionCreate, async (interaction) => {
@@ -32,7 +38,13 @@ yargs(hideBin(process.argv))
         (command) => command.data.name === interaction.commandName,
       );
       if (command !== undefined) {
-        await command.execute(interaction);
+        try {
+          await command.execute(interaction);
+        } catch (err) {
+          console.error(
+            `Failed to execute handler for command: ${command.data.name}, error: ${getErrorMessage(err)}`,
+          );
+        }
       } else {
         console.warn(
           `Could not find handler for command: ${interaction.commandName}`,
@@ -40,7 +52,11 @@ yargs(hideBin(process.argv))
       }
     });
 
-    client.login(await getToken());
+    try {
+      await client.login(await getToken());
+    } catch (err) {
+      console.error(`Failed to log in: ${getErrorMessage(err)}`);
+    }
   })
   .demandCommand(1)
   .parse();
