6e4faee32ab636117138d17bb443bb384fa43328
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-06-10 18:17:31 +0700

    test: simplify and clarify the List Jobs command tests

--- src/commands/jobs/list.test.ts
+++ src/commands/jobs/list.test.ts
@@ -1,58 +1,50 @@
 import { jest } from "@jest/globals";
-import { CacheType, ChatInputCommandInteraction } from "discord";
-import { formatRssFeedItem } from "../../feed.js";
-
-const rssFeedItems = [
-  {
-    title: "First Job",
-    link: "https://www.upwork.com/link-to-first-job",
-    contentSnippet: "Description of the first job",
-    guid: "1",
-  },
-  {
-    title: "Second Job",
-    link: "https://www.upwork.com/link-to-second-job",
-    contentSnippet: "Description of the second job",
-    guid: "2",
-  },
-];
-
-jest.unstable_mockModule("../../feed.js", () => ({
-  formatRssFeedItem: formatRssFeedItem,
-  tryToFetchRssFeedFromUrl: async (url: string) => {
-    if (url === "https://www.upwork.com/some-rss") {
-      return rssFeedItems;
-    }
-    return [];
-  },
-}));
-
-const interaction = {
-  channel: {
-    send: jest.fn(),
-  },
-  options: {
-    getString: (key: string) => {
-      return key === "url" ? "https://www.upwork.com/some-rss" : "";
-    },
-  },
-  reply: jest.fn(),
-};
 
 it("should list jobs from the given RSS feed URL", async () => {
+  jest.unstable_mockModule("../../feed.js", () => ({
+    // Mock the RSS feed item to be formatted with just the item's title.
+    formatRssFeedItem: (item: { title: string }) => item.title,
+
+    // Mock the RSS feed items that will be fetched.
+    tryToFetchRssFeedFromUrl: async (url: string) => {
+      if (url === "https://www.upwork.com/some-rss") {
+        return [
+          { title: "First Job", guid: "1" },
+          { title: "Second Job", guid: "2" },
+        ];
+      }
+      return [];
+    },
+  }));
+
+  // Mock the function for sending the message.
+  const tryToSendMessageToChannel = jest.fn();
+  jest.unstable_mockModule("../../message.js", () => ({
+    tryToSendMessageToChannel,
+  }));
+
+  // Construct a mocked interaction.
+  const interaction = {
+    channel: {},
+    options: {
+      getString: (key: string) => {
+        if (key === "url") return "https://www.upwork.com/some-rss";
+        return "";
+      },
+    },
+    reply: jest.fn(),
+  };
+
+  // Execute the command with a mocked interaction.
   const ListJobsCommand = (await import("./list.js")).default;
+  await ListJobsCommand.execute(interaction as any);
 
-  await ListJobsCommand.execute(
-    interaction as unknown as ChatInputCommandInteraction<CacheType>,
-  );
+  // Expect to reply with the correct message.
+  expect(interaction.reply.mock.calls).toEqual([[`Listing 2 jobs:`]]);
 
-  expect(interaction.reply).toHaveBeenCalledTimes(1);
-  expect(interaction.reply).toHaveBeenLastCalledWith(
-    `Listing ${rssFeedItems.length} jobs:`,
-  );
-
-  expect(interaction.channel.send).toHaveBeenCalledTimes(rssFeedItems.length);
-  expect(interaction.channel.send.mock.calls).toEqual(
-    rssFeedItems.map((item) => [formatRssFeedItem(item)]),
-  );
+  // Expect to send the correct messages to the channel.
+  expect(tryToSendMessageToChannel.mock.calls).toEqual([
+    ["First Job", interaction.channel],
+    ["Second Job", interaction.channel],
+  ]);
 });
