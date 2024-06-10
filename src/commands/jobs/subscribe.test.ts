10ea76308efff85e158e0d61b9f7e3044f36ae73
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-06-10 18:37:19 +0700

    test: simplify and clarify the Subscribe Jobs command tests

--- src/commands/jobs/subscribe.test.ts
+++ src/commands/jobs/subscribe.test.ts
@@ -1,79 +1,85 @@
 import { jest } from "@jest/globals";
-import { CacheType, ChatInputCommandInteraction } from "discord";
-
-jest.useFakeTimers();
-
-const rssFeedItems = [
-  { title: "First Job", guid: "1" },
-  { title: "Second Job", guid: "2" },
-  { title: "Third Job", guid: "3" },
-];
-
-jest.unstable_mockModule("../../feed.js", () => ({
-  formatRssFeedItem: (item: { title: string }) => item.title,
-  tryToFetchRssFeedFromUrl: async (url: string) => {
-    if (url === "https://www.upwork.com/some-rss") {
-      return rssFeedItems;
-    }
-    return [];
-  },
-}));
-
-jest.unstable_mockModule("../../store.js", () => ({
-  default: new (class {
-    data: string[] = [];
-    update(fn: (data: string[]) => void) {
-      fn(this.data);
-    }
-  })(),
-}));
-
-const interaction = {
-  channel: {
-    send: jest.fn(),
-  },
-  options: {
-    getString: (key: string) => {
-      return key === "url" ? "https://www.upwork.com/some-rss" : "";
-    },
-  },
-  reply: jest.fn(),
-};
 
 it("should subscribe jobs from the given RSS feed URL", async () => {
+  jest.useFakeTimers();
+
+  // Construct the RSS feed items that will be fetched.
+  const rssFeedItems = [
+    { title: "First Job", guid: "1" },
+    { title: "Second Job", guid: "2" },
+    { title: "Third Job", guid: "3" },
+  ];
+
+  jest.unstable_mockModule("../../feed.js", () => ({
+    // Mock the RSS feed item to be formatted with just the item's title.
+    formatRssFeedItem: (item: { title: string }) => item.title,
+
+    // Mock the RSS feed items that will be fetched.
+    tryToFetchRssFeedFromUrl: async (url: string) => {
+      if (url === "https://www.upwork.com/some-rss") return rssFeedItems;
+      return [];
+    },
+  }));
+
+  // Mock the function for sending the message.
+  const tryToSendMessageToChannel = jest.fn().mockReturnValue(true);
+  jest.unstable_mockModule("../../message.js", () => ({
+    tryToSendMessageToChannel,
+  }));
+
+  // Mock the database for storing listed jobs.
+  jest.unstable_mockModule("../../store.js", () => ({
+    default: new (class {
+      // Initialize the job with GUID `2` to be already listed.
+      data: string[] = ["2"];
+
+      update(fn: (data: string[]) => void) {
+        fn(this.data);
+      }
+    })(),
+  }));
+
+  // Construct a mocked interaction.
+  const interaction = {
+    channel: {},
+    options: {
+      getString: (key: string) => {
+        if (key === "url") return "https://www.upwork.com/some-rss";
+        return "";
+      },
+    },
+    reply: jest.fn(),
+  };
+
+  // Execute the command with a mocked interaction.
   const SubscribeJobsCommand = (await import("./subscribe.js")).default;
-  const store = (await import("../../store.js")).default;
+  await SubscribeJobsCommand.execute(interaction as any);
 
-  // Initialize the job with GUID `2` to be already listed.
-  store.update((data) => data.push("2"));
-
-  await SubscribeJobsCommand.execute(
-    interaction as unknown as ChatInputCommandInteraction<CacheType>,
-  );
-
-  expect(interaction.reply).toHaveBeenCalledTimes(1);
-  expect(interaction.reply).toHaveBeenLastCalledWith(
-    "Subscribed to: <https://www.upwork.com/some-rss>",
-  );
-
-  expect(interaction.channel.send.mock.calls).toEqual([
-    ["First Job"],
-    ["Third Job"],
+  // Expect to reply with the correct message.
+  expect(interaction.reply.mock.calls).toEqual([
+    ["Subscribed to: <https://www.upwork.com/some-rss>"],
   ]);
 
+  // Expect to send the correct messages to the channel.
+  expect(tryToSendMessageToChannel.mock.calls).toEqual([
+    ["First Job", interaction.channel],
+    ["Third Job", interaction.channel],
+  ]);
+
+  // Advance the time without updating the RSS feed items.
+  tryToSendMessageToChannel.mockClear();
   await jest.advanceTimersByTimeAsync(60000);
 
-  expect(interaction.channel.send.mock.calls).toEqual([
-    ["First Job"],
-    ["Third Job"],
-  ]);
+  // Expect to send no messages to the channel.
+  expect(tryToSendMessageToChannel.mock.calls).toEqual([]);
 
+  // Advance the time with updating the RSS feed items.
   rssFeedItems.push({ title: "Fourth Job", guid: "4" });
+  tryToSendMessageToChannel.mockClear();
   await jest.advanceTimersByTimeAsync(60000);
 
-  expect(interaction.channel.send.mock.calls).toEqual([
-    ["First Job"],
-    ["Third Job"],
-    ["Fourth Job"],
+  // Expect to only send the new messages to the channel.
+  expect(tryToSendMessageToChannel.mock.calls).toEqual([
+    ["Fourth Job", interaction.channel],
   ]);
 });
