be0db78f87e8423f630d1ff2ba46d933bf8e7682
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-06-11 18:37:24 +0700

    feat: add `markJobAsPosted` function

--- src/store/jobs.test.ts
+++ src/store/jobs.test.ts
@@ -1,25 +1,29 @@
 import { jest } from "@jest/globals";
 
 // Mock the database.
-const db = new (class {
-  data: string[] = [];
-  update(fn: (data: string[]) => void) {
-    fn(this.data);
-  }
-})();
-jest.unstable_mockModule("./db.js", () => ({ default: db }));
+jest.unstable_mockModule("./db.js", () => ({
+  default: new (class {
+    data: string[] = [];
+    async update(fn: (data: string[]) => void) {
+      fn(this.data);
+    }
+  })(),
+}));
 
 it("should checks whether a job has already been posted", async () => {
-  const { isJobPosted } = await import("./jobs.js");
+  const { isJobPosted, markJobAsPosted } = await import("./jobs.js");
 
   expect(isJobPosted("some-job")).toBe(false);
 
-  db.update((data) => data.push("some-job"));
+  await markJobAsPosted("some-job");
   expect(isJobPosted("some-job")).toBe(true);
 });
 
 it("should checks whether an undefined job has already been posted", async () => {
-  const { isJobPosted } = await import("./jobs.js");
+  const { isJobPosted, markJobAsPosted } = await import("./jobs.js");
 
+  expect(isJobPosted(undefined)).toBe(false);
+
+  await markJobAsPosted(undefined);
   expect(isJobPosted(undefined)).toBe(false);
 });
--- src/store/jobs.ts
+++ src/store/jobs.ts
@@ -10,3 +10,17 @@ export function isJobPosted(jobId: string | undefined): boolean {
   if (jobId === undefined) return false;
   return db.data.includes(jobId);
 }
+
+/**
+ * Marks a job with a specific ID as posted.
+ *
+ * @param jobId - The ID of the job.
+ * @returns A promise that resolves to nothing.
+ */
+export async function markJobAsPosted(
+  jobId: string | undefined,
+): Promise<void> {
+  if (jobId !== undefined) {
+    await db.update((data) => data.push(jobId));
+  }
+}
